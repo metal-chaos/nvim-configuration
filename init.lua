895a9f54f97fbed48187abdab50c4b6ea19163b2
uuta <nightrage.metal66@gmail.com>
2022-07-31 02:13:14 +0900

    Replaced init.vim with init.lua

---
+++ init.lua
@@ -0,0 +1,229 @@
+local vim = vim
+local api = vim.api
+local o = vim.o
+vim.g.mapleader = ";"
+
+-- Basic configuration
+o.number = true
+vim.opt.clipboard:append{'unnamedplus'}
+
+-- Indent visible
+o.list = true
+vim.opt.listchars:append({ tab="»-", trail="-", eol="↲", extends="»", precedes="«", nbsp="%" })
+
+-- Set keymap
+vim.keymap.set('n', '<Leader>j', '<Plug>(jumpcursor-jump)')
+vim.keymap.set('n', '<Leader>q', '<cmd>:q<cr>')
+vim.keymap.set('n', '<Leader>w', '<cmd>:w<cr>')
+vim.keymap.set('n', '<Leader>wq', '<cmd>:wq<cr>')
+vim.keymap.set('n', '<Leader>2', '<cmd>vi"y<cr>')
+vim.keymap.set('n', "<Leader>7", "<cmd>vi'y<cr>")
+
+--- LSP
+vim.keymap.set('n', '<Leader>K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', '<Leader>s', '<cmd>lua vim.lsp.buf.format()<CR>')
+vim.keymap.set('n', '<Leader>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n', '<Leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', '<Leader>D', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set('n', '<Leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n', '<Leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+vim.keymap.set('n', '<Leader>gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n', '<Leader>ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n', '<Leader>ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', '<Leader>g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', '<Leader>g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+
+--- Telescope
+vim.keymap.set('n', '<Leader>p', '<cmd>Telescope find_files hidden=true theme=get_dropdown<CR>')
+vim.keymap.set('n', '<Leader>gr', '<cmd>Telescope live_grep theme=get_dropdown<CR>')
+vim.keymap.set('n', '<Leader>b', '<cmd>Telescope buffers theme=get_dropdown<CR>')
+vim.keymap.set('n', '<Leader>h', '<cmd>Telescope oldfiles theme=get_dropdown<CR>')
+vim.keymap.set('n', '<Leader>gb', '<cmd>Telescope git_branches theme=get_dropdown<CR>')
+
+--- Fern
+vim.keymap.set('n', '<Leader>f', '<cmd>Fern . -reveal=% -drawer<CR>')
+
+--- Mason
+vim.keymap.set('n', '<Leader>M', '<cmd>:Mason<cr>')
+
+
+vim.cmd[[packadd packer.nvim]]
+vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
+
+require('packer').startup(function(use)
+  -- 起動時に読み込むプラグインは名前を書くだけです
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-repeat'
+  use 'neovim/nvim-lspconfig'
+  --- LSP
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/vim-vsnip"
+
+  --- Colorscheme
+  use "tanvirtin/monokai.nvim"
+  use "itchyny/lightline.vim"
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'norcalli/nvim-colorizer.lua'
+
+  --- Cursor mover
+  use "skanehira/jumpcursor.vim"
+
+  --- Git Blame
+  use "f-person/git-blame.nvim"
+
+  --- Telescope
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.0' }
+
+  --- Fern
+  use "lambdalisue/fern.vim"
+
+  -- opt オプションを付けると遅延読み込みになります。
+  -- この場合は opt だけで読み込む契機を指定していないため、
+  -- `packadd` コマンドを叩かない限り読み込まれることはありません。
+  use{'wbthomason/packer.nvim', opt = true}
+  -- packer.nvim 自体を遅延読み込みにする理由はまた後ほど。
+
+  -- コマンドを叩いたときに読み込む。
+  use{'rhysd/git-messenger.vim', opt = true, cmd = {'GitMessenger'}}
+
+  -- 特定のイベントで読み込む
+  use{'tpope/vim-unimpaired', opt = true, event = {'FocusLost', 'CursorHold'}}
+
+  -- 特定のファイルタイプのファイルを開いたら読み込む
+  use{'fatih/vim-go', opt = true, ft = {'go'}}
+
+  -- 特定のキーを叩いたら読み込む
+  -- この例ではノーマルモードの <CR> にマッピングしていますが、
+  -- モードを指定する場合はテーブルを入れ子にします。
+  -- keys = {
+  --   {'n', '<CR>'},
+  --   {'v', '<CR>'},
+  -- }
+  use{
+    'arecarn/vim-fold-cycle',
+    opt = true,
+    keys = {'<CR>'},
+  }
+
+  -- 特定の VimL 関数を呼ぶと読み込む
+  -- この例だと、任意の場所で Artify('hoge', 'bold') のように呼び出された時に、
+  -- このプラグインも読み込まれます。
+  use{'sainnhe/artify.vim', opt = true, fn = {'Artify'}}
+
+  -- 実は opt = true は省略できます。読み込む契機（この例では cmd）を指定すると、
+  -- 自動的に遅延読み込みとみなされます。
+  use{
+    'ellisonleao/glow.nvim',
+    cmd = {'Glow', 'GlowInstall'},
+    -- run オプションを指定すると、インストール時・更新時に
+    -- 実行するコマンドを指定できます。
+    run = [[:GlowInstall]],
+    -- 先頭に : がついていないなら bash -c '...' で実行されます。
+    -- run = [[npm install]],
+    -- 関数も指定可能です。
+    -- run = function() vim.cmd[[GlowInstall]] end,
+  }
+
+  -- 条件が真の時のみ読み込みます。条件は起動時に毎回判定されます。
+  use{
+    'thinca/vim-fontzoom',
+    cond = [[vim.fn.has'gui' == 1]], -- GUI の時のみ読み込む。
+    -- 関数も指定できます。
+    -- conf = function() return vim.fn.has'gui' == 1 end,
+  }
+
+  -- 依存関係も管理できます。vim-prettyprint は
+  -- capture.vim が読み込まれる前に、自動的に packadd されます。
+  use{
+    'tyru/capture.vim',
+    requires = {
+      {'thinca/vim-prettyprint', cmd = {'PP', 'PrettyPrint'}},
+    },
+    cmd = {'Capture'},
+  }
+
+end)
+
+-- 1. LSP Sever management
+require('mason').setup()
+require('mason-lspconfig').setup_handlers({ function(server)
+  local opt = {
+    -- -- Function executed when the LSP server startup
+    -- on_attach = function(client, bufnr)
+    --   local opts = { noremap=true, silent=true }
+    --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    --   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
+    -- end,
+    capabilities = require('cmp_nvim_lsp').update_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+    )
+  }
+  require('lspconfig')[server].setup(opt)
+end })
+
+-- 2. build-in LSP function
+-- keyboard shortcut
+-- LSP handlers
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
+)
+-- Reference highlight
+vim.cmd [[
+highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+augroup lsp_document_highlight
+  autocmd!
+  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+augroup END
+]]
+
+-- 3. completion (hrsh7th/nvim-cmp)
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    -- { name = "buffer" },
+    -- { name = "path" },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ['<C-l>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  }),
+  experimental = {
+    ghost_text = true,
+  },
+})
+
+-- Color Scheme
+-- https://github.com/tanvirtin/monokai.nvim
+require('monokai').setup { }
+
+-- Code highlighter
+require('nvim-treesitter.configs').setup {
+  highlight = {
+    enable = true,  -- syntax highlightを有効にする
+    disable = {     -- 一部の言語では無効にする
+      'toml',
+    }
+  },
+  ensure_installed = 'all', -- :TSInstall allと同じ
+  -- ensure_installed = 'maintained' とすることもできる
+}
+
+-- Color highlighter
+-- https://github.com/norcalli/nvim-colorizer.lua
+require('colorizer').setup()
---
+++ plugin/packer_compiled.lua
@@ -0,0 +1,267 @@
+-- Automatically generated packer.nvim plugin loader code
+
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
+end
+
+vim.api.nvim_command('packadd packer.nvim')
+
+local no_errors, error_msg = pcall(function()
+
+  local time
+  local profile_info
+  local should_profile = false
+  if should_profile then
+    local hrtime = vim.loop.hrtime
+    profile_info = {}
+    time = function(chunk, start)
+      if start then
+        profile_info[chunk] = hrtime()
+      else
+        profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+      end
+    end
+  else
+    time = function(chunk, start) end
+  end
+  
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+
+  _G._packer = _G._packer or {}
+  _G._packer.profile_output = results
+end
+
+time([[Luarocks path setup]], true)
+local package_path_str = "/Users/yutaaoki/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/yutaaoki/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/yutaaoki/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/yutaaoki/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/yutaaoki/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
+
+time([[try_loadstring definition]], false)
+time([[Defining packer_plugins]], true)
+_G.packer_plugins = {
+  ["artify.vim"] = {
+    loaded = false,
+    needs_bufread = false,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/artify.vim",
+    url = "https://github.com/sainnhe/artify.vim"
+  },
+  ["capture.vim"] = {
+    commands = { "Capture" },
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/capture.vim",
+    url = "https://github.com/tyru/capture.vim"
+  },
+  ["cmp-nvim-lsp"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
+  },
+  ["git-blame.nvim"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/git-blame.nvim",
+    url = "https://github.com/f-person/git-blame.nvim"
+  },
+  ["git-messenger.vim"] = {
+    commands = { "GitMessenger" },
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/git-messenger.vim",
+    url = "https://github.com/rhysd/git-messenger.vim"
+  },
+  ["glow.nvim"] = {
+    commands = { "Glow", "GlowInstall" },
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/glow.nvim",
+    url = "https://github.com/ellisonleao/glow.nvim"
+  },
+  ["jumpcursor.vim"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/jumpcursor.vim",
+    url = "https://github.com/skanehira/jumpcursor.vim"
+  },
+  ["lightline.vim"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/lightline.vim",
+    url = "https://github.com/itchyny/lightline.vim"
+  },
+  ["mason-lspconfig.nvim"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
+  },
+  ["monokai.nvim"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/monokai.nvim",
+    url = "https://github.com/tanvirtin/monokai.nvim"
+  },
+  ["nvim-cmp"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    url = "https://github.com/hrsh7th/nvim-cmp"
+  },
+  ["nvim-colorizer.lua"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    url = "https://github.com/norcalli/nvim-colorizer.lua"
+  },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
+  ["packer.nvim"] = {
+    loaded = false,
+    needs_bufread = false,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["vim-fold-cycle"] = {
+    keys = { { "", "<CR>" } },
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/vim-fold-cycle",
+    url = "https://github.com/arecarn/vim-fold-cycle"
+  },
+  ["vim-fontzoom"] = {
+    cond = { "return vim.fn.has'gui' == 1" },
+    loaded = false,
+    needs_bufread = false,
+    only_cond = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/vim-fontzoom",
+    url = "https://github.com/thinca/vim-fontzoom"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-go"] = {
+    loaded = false,
+    needs_bufread = true,
+    only_cond = false,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/vim-go",
+    url = "https://github.com/fatih/vim-go"
+  },
+  ["vim-prettyprint"] = {
+    commands = { "PP", "PrettyPrint" },
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/vim-prettyprint",
+    url = "https://github.com/thinca/vim-prettyprint"
+  },
+  ["vim-repeat"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/vim-repeat",
+    url = "https://github.com/tpope/vim-repeat"
+  },
+  ["vim-unimpaired"] = {
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/vim-unimpaired",
+    url = "https://github.com/tpope/vim-unimpaired"
+  },
+  ["vim-vsnip"] = {
+    loaded = true,
+    path = "/Users/yutaaoki/.local/share/nvim/site/pack/packer/start/vim-vsnip",
+    url = "https://github.com/hrsh7th/vim-vsnip"
+  }
+}
+
+time([[Defining packer_plugins]], false)
+-- Conditional loads
+time([[Conditional loading of vim-fontzoom]], true)
+  require("packer.load")({"vim-fontzoom"}, {}, _G.packer_plugins)
+time([[Conditional loading of vim-fontzoom]], false)
+
+-- Command lazy-loads
+time([[Defining lazy-load commands]], true)
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file GitMessenger lua require("packer.load")({'git-messenger.vim'}, { cmd = "GitMessenger", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Capture lua require("packer.load")({'capture.vim'}, { cmd = "Capture", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file PP lua require("packer.load")({'vim-prettyprint'}, { cmd = "PP", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Glow lua require("packer.load")({'glow.nvim'}, { cmd = "Glow", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file GlowInstall lua require("packer.load")({'glow.nvim'}, { cmd = "GlowInstall", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file PrettyPrint lua require("packer.load")({'vim-prettyprint'}, { cmd = "PrettyPrint", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+time([[Defining lazy-load commands]], false)
+
+-- Keymap lazy-loads
+time([[Defining lazy-load keymaps]], true)
+vim.cmd [[noremap <silent> <CR> <cmd>lua require("packer.load")({'vim-fold-cycle'}, { keys = "<lt>CR>", prefix = "" }, _G.packer_plugins)<cr>]]
+time([[Defining lazy-load keymaps]], false)
+
+vim.cmd [[augroup packer_load_aucmds]]
+vim.cmd [[au!]]
+  -- Filetype lazy-loads
+time([[Defining lazy-load filetype autocommands]], true)
+vim.cmd [[au FileType go ++once lua require("packer.load")({'vim-go'}, { ft = "go" }, _G.packer_plugins)]]
+time([[Defining lazy-load filetype autocommands]], false)
+  -- Event lazy-loads
+time([[Defining lazy-load event autocommands]], true)
+vim.cmd [[au FocusLost * ++once lua require("packer.load")({'vim-unimpaired'}, { event = "FocusLost *" }, _G.packer_plugins)]]
+vim.cmd [[au CursorHold * ++once lua require("packer.load")({'vim-unimpaired'}, { event = "CursorHold *" }, _G.packer_plugins)]]
+time([[Defining lazy-load event autocommands]], false)
+  -- Function lazy-loads
+time([[Defining lazy-load function autocommands]], true)
+vim.cmd[[au FuncUndefined Artify ++once lua require("packer.load")({'artify.vim'}, {}, _G.packer_plugins)]]
+time([[Defining lazy-load function autocommands]], false)
+vim.cmd("augroup END")
+vim.cmd [[augroup filetypedetect]]
+time([[Sourcing ftdetect script at: /Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/vim-go/ftdetect/gofiletype.vim]], true)
+vim.cmd [[source /Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/vim-go/ftdetect/gofiletype.vim]]
+time([[Sourcing ftdetect script at: /Users/yutaaoki/.local/share/nvim/site/pack/packer/opt/vim-go/ftdetect/gofiletype.vim]], false)
+vim.cmd("augroup END")
+if should_profile then save_profiles() end
+
+end)
+
+if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
+end
