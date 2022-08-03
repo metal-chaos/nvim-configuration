cae3488df549c019af1af8de85cec77716be25d6
uuta <nightrage.metal66@gmail.com>
2022-08-03 11:07:34 +0900

    Adjusted the size of indent

--- init.lua
+++ init.lua
@@ -6,6 +6,10 @@ vim.g.mapleader = ";"
 o.number = true
 vim.opt.clipboard:append({ unnamedplus = true })
 vim.opt.termguicolors = true
+vim.o.hidden = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
 -- Set keymap
 vim.keymap.set('n', '<Leader>j', '<Plug>(jumpcursor-jump)')
