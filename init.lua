46f69913871f820214ac85e1d60afa8c29615f51
uuta <nightrage.metal66@gmail.com>
2022-08-05 17:46:12 +0900

    Temporary commit

--- init.lua
+++ init.lua
@@ -284,44 +284,30 @@ end)
 -- Formatter
 -- https://github.com/jose-elias-alvarez/null-ls.nvim
 -- https://zenn.dev/nazo6/articles/c2f16b07798bab
+-- Maybe this way? -> https://github.com/jose-elias-alvarez/null-ls.nvim/issues/844
 local null_ls = require("null-ls")
-null_ls.setup {
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+require("null-ls").setup({
 	sources = {
 		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.phpcsfixer
-	}
-}
-
--- 1. LSP Sever management
-require('mason').setup()
-require('mason-lspconfig').setup_handlers({ function(server)
-	local lsp_formatting = function(bufnr)
-		vim.lsp.buf.format({
-			filter = function(client)
-				-- apply whatever logic you want (in this example, we'll only use null-ls)
-				return client.name == "null-ls"
-			end,
-			bufnr = bufnr,
-		})
-	end
-
-	-- if you want to set up formatting on save, you can use this as a callback
-	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-	-- add to your shared on_attach callback
-	local on_attach = function(client, bufnr)
+		null_ls.builtins.formatting.goimports,
+	},
+	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = augroup,
 				buffer = bufnr,
-				callback = function()
-					lsp_formatting(bufnr)
-				end,
+				callback = vim.lsp.buf.formatting_sync,
 			})
 		end
-	end
-	require('lspconfig')[server].setup { on_attach }
+	end,
+})
+
+-- 1. LSP Sever management
+require('mason').setup()
+require('mason-lspconfig').setup_handlers({ function(server)
+	require('lspconfig')[server].setup {}
 end })
 
 -- 2. build-in LSP function
