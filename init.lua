1abae7c84ce72c7f2ec3beb4a9b7a2e9791a5812
uuta <nightrage.metal66@gmail.com>
2023-02-23 19:38:02 +0900

    Added noice.nvim

--- init.lua
+++ init.lua
@@ -447,6 +447,40 @@ require('packer').startup(function(use)
         config = function() require("impatient") end
     }
 
+    -- Noice
+    -- Customize here if bothersome notification appeared
+    -- https://github.com/folke/noice.nvim
+    use({
+        "folke/noice.nvim",
+        config = function()
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true
+                    }
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false -- add a border to hover docs and signature help
+                }
+            })
+        end,
+        requires = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim", -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify"
+        }
+    })
+
     -- opt オプションを付けると遅延読み込みになります。
     -- この場合は opt だけで読み込む契機を指定していないため、
     -- `packadd` コマンドを叩かない限り読み込まれることはありません。
