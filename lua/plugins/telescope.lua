47b247adc32a7fc8553aae729de64b265e3f2fef
uuta <nightrage.metal66@gmail.com>
2023-05-13 13:02:36 +0900

    Added mappings to easily search strings with filename extensions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,5 +11,39 @@ return {
     -- Search strings with filters such as a directory
     -- @see https://github.com/nvim-telescope/telescope-live-grep-args.nvim
     dependencies = {{"nvim-telescope/telescope-live-grep-args.nvim"}},
-    config = function() require("telescope").load_extension("live_grep_args") end
+    config = function()
+        local lga_actions = require("telescope-live-grep-args.actions")
+        require("telescope").setup({
+            extensions = {
+                live_grep_args = {
+                    auto_quoting = true, -- enable/disable auto-quoting
+                    -- define mappings, e.g.
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = lga_actions.quote_prompt(),
+                            ["<C-i>"] = lga_actions.quote_prompt({
+                                postfix = " --iglob "
+                            }),
+                            ["<C-g>"] = lga_actions.quote_prompt({
+                                postfix = " --iglob *go"
+                            }),
+                            ["<C-r>"] = lga_actions.quote_prompt({
+                                postfix = " --iglob *tsx"
+                            }),
+                            ["<C-t>"] = lga_actions.quote_prompt({
+                                postfix = " --iglob *ts"
+                            }),
+                            ["<C-p>"] = lga_actions.quote_prompt({
+                                postfix = " --iglob *php"
+                            })
+                        }
+                    }
+                    -- ... also accepts theme settings, for example:
+                    -- theme = "dropdown", -- use dropdown theme
+                    -- theme = { }, -- use own theme spec
+                    -- layout_config = { mirror=true }, -- mirror preview pane
+                }
+            }
+        })
+    end
 }
