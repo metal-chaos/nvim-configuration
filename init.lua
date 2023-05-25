d67c7dfb1caa0ec45cddd1c6329b9964dbaa05b0
uuta <nightrage.metal66@gmail.com>
2023-05-25 14:26:33 +0900

    Added vim.loader.enable() into init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- Fast load Neovim
+-- https://github.com/neovim/neovim/pull/22668
+vim.loader.enable()
+
 require "core.options"
 require "core.keymaps"
 require "lazy-setup"
--- lua/plugins/impatient.lua
+++
@@ -1,6 +0,0 @@
--- impatient.nvim: Speed up loading Lua modules in Neovim to improve startup time.
--- https://github.com/lewis6991/impatient.nvim
-return {
-    "lewis6991/impatient.nvim",
-    config = function() require("impatient") end
-}
