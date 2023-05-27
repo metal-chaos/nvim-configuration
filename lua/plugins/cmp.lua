928a97a099d8118471d9e6f77bc34936068153f4
uuta <nightrage.metal66@gmail.com>
2023-05-27 16:14:52 +0900

    Added completion and documentation in window in cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,12 +1,7 @@
 -- https://github.com/hrsh7th/nvim-cmp
 -- It didn't work when adding cmp to config in this file so separated it into core/cmp.lua
 return {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-vsnip",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-vsnip",
-	"hrsh7th/vim-vsnip",
+    "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path",
+    "hrsh7th/cmp-vsnip", "hrsh7th/cmp-cmdline", "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-vsnip", "hrsh7th/vim-vsnip"
 }
