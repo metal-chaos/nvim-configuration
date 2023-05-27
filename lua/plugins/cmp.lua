1e3992f1b34facaad1dbd46af2f2542f52af594e
uuta <nightrage.metal66@gmail.com>
2023-05-27 15:56:25 +0900

    Added cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,27 +1,12 @@
+-- https://github.com/hrsh7th/nvim-cmp
+-- It didn't work when adding cmp to config in this file so separated it into core/cmp.lua
 return {
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-vsnip",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-vsnip",
 	"hrsh7th/vim-vsnip",
-    config = function()
-		local cmp = require("cmp")
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    vim.fn["vsnip#anonymous"](args.body)
-                end
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm({select = true})
-            }),
-            sources = cmp.config.sources({
-                {name = "nvim_lsp"}, {name = "vsnip"}
-            }, {{name = "buffer"}})
-        })
-    end
 }
