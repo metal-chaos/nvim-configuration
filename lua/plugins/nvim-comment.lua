943335a3936087a1bdcf3e4041bafb18bdfa5ac7
uuta <nightrage.metal66@gmail.com>
2023-06-01 17:00:35 +0900

    Installed nvim-ts-context-commentstring and reflected its configuration

--- lua/plugins/nvim-comment.lua
+++ lua/plugins/nvim-comment.lua
@@ -7,5 +7,11 @@
 -- gcic uncomment commented block
 return {
     "terrortylor/nvim-comment",
-    config = function() require("nvim_comment").setup({}) end
+    config = function()
+        require("nvim_comment").setup({
+            hook = function()
+                require('ts_context_commentstring.internal').update_commentstring()
+            end
+        })
+    end
 }
