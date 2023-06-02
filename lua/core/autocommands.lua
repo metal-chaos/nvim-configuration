60d90c5671e4257b9ddd133c2704f2a456a3438a
uuta <nightrage.metal66@gmail.com>
2023-06-02 18:37:00 +0900

    Added auto_cmd to start normal mode after opening a file with telescope

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -27,3 +27,15 @@ api.nvim_create_autocmd({"BufReadPost"}, {
     pattern = {"*"},
     callback = function() vim.api.nvim_exec('silent! normal! g`"zv', false) end
 })
+
+-- To stop beginning from insert mode
+-- https://github.com/nvim-telescope/telescope.nvim/issues/2027#issuecomment-1561836585
+api.nvim_create_autocmd("WinLeave", {
+    callback = function()
+        if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true,
+                                                                 false, true),
+                                  "i", false)
+        end
+    end
+})
