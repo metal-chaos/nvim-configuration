04d4236afb7c3d304e97508acadf4808f41c1da8
uuta <nightrage.metal66@gmail.com>
2024-04-13 17:55:10 +0900

    Add NeoSolarized color scheme and configuration

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,9 +1,1 @@
-local g = vim.g
-
--- Color Scheme
-g.tokyodark_transparent_background = false
-g.tokyodark_enable_italic_comment = true
-g.tokyodark_enable_italic = false
-g.tokyodark_color_gamma = "0.9"
-
-vim.cmd("colorscheme onedark_dark")
+vim.cmd("colorscheme NeoSolarized")
