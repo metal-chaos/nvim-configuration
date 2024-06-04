01abc360688b1c2761ad9f26fbfc6e547de8c9f3
uuta <nightrage.metal66@gmail.com>
2024-06-04 12:18:16 +0900

    Update LuaSnip commit hash to 2b6860d15aaab01d3fb90859c0ba97f20ad7bc5f

--- lua/plugins/lua-ship.lua
+++ lua/plugins/lua-ship.lua
@@ -2,8 +2,6 @@
 return {
     {
         "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        commit = "de1a287c9cb525ae52bc846e8f6207e5ef1da5ac",
         -- install jsregexp (optional!).
         build = "make install_jsregexp"
     }
